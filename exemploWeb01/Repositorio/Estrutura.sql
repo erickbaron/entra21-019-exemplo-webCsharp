f2e6c408238718c77a3c0304335e02a71f8f539a
Erick Eduardo Baron <erickbaron123456@gmail.com>
2019-07-03 11:32:16 -0300

    Terminado, faltando desing

---
+++ exemploWeb01/Repositorio/Estrutura.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE estoques;
+CREATE TABLE estoques(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(100),
+	valor DECIMAL (8,2),
+	quantidade INT
+	
+);
+
+INSERT INTO estoques (nome, valor, quantidade)
+VALUES ('Biela', 150, 20)
